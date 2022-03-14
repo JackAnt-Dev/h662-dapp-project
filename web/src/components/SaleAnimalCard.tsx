602adfb7523da0568192ce23ad4c9016e8b57d3b
JackAnt-Dev <jactant.dev@gmail.com>
2022-03-14 21:26:18 +0900

    판매자 정보 가져오기 완료

--- web/src/components/SaleAnimalCard.tsx
+++ web/src/components/SaleAnimalCard.tsx
@@ -1,23 +1,45 @@
 import { Box, Button, Text } from "@chakra-ui/react";
-import React, { FC } from "react";
-import { web3 } from "../web3Config";
+import React, { FC, useEffect, useState } from "react";
+import { mintAnimalTokenContract, web3 } from "../web3Config";
 import AnimalCard from "./AnimalCard";
 
 interface SaleAnimalCardProps {
   animalType: string;
   animalPrice: string;
+  animalTokenId: string;
+  account: string;
 }
 
 const SaleAnimalCard: FC<SaleAnimalCardProps> = ({
   animalType,
   animalPrice,
+  animalTokenId,
+  account,
 }) => {
+  const [isBuyable, setIsBuyable] = useState<boolean>(false);
+
+  const getAnimalTokenOwner = async () => {
+    try {
+      const response = await mintAnimalTokenContract.methods
+        .ownerOf(animalTokenId)
+        .call();
+
+      setIsBuyable(response.toLocaleLowerCase() != account.toLocaleLowerCase());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAnimalTokenOwner();
+  }, []);
+
   return (
     <Box textAlign="center" w={150}>
       <AnimalCard animalType={animalType} />
       <Box>
         <Text d="inline-block">{web3.utils.fromWei(animalPrice)} Ethers</Text>
-        <Button size="sm" colorScheme="green">
+        <Button size="sm" colorScheme="green" disabled={!isBuyable}>
           Buy
         </Button>
       </Box>
